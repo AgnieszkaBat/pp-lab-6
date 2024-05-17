2285f2f256c6a675b470ad00eb1cef94de68ffd5
AgnieszkaBat <166222350+AgnieszkaBat@users.noreply.github.com>
2024-05-17 20:06:24 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,68 @@
+public interface Employable {
+    void work();
+}
+
+public abstract class Employee implements Employable {
+    private String name;
+    private double salary;
+    private int id;
+    private String hireDate;
+    private String position;
+
+    public Employee(String name, double salary, int id, String hireDate, String position) {
+        this.name = name;
+        this.salary = salary;
+        this.id = id;
+        this.hireDate = hireDate;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    @Override
+    public abstract void work();
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
+
+public class Manager extends Employee {
+    public Manager(String name, double salary, int id) {
+        super(name, salary, id, null, null);
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Manager " + getName() + " is managing...");
+    }
+}
+
+public final class Worker extends Employee {
+    public Worker(String name, double salary, int id) {
+        super(name, salary, id, null, null);
+    }
+
+    @Override
+    public void work() {
+        System.out.println("Worker " + getName() + " is working...");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {

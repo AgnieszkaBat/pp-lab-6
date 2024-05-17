d00cc1d286ba1b4a11e2a904f7345ad17f109ecc
AgnieszkaBat <166222350+AgnieszkaBat@users.noreply.github.com>
2024-05-17 19:58:43 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,27 @@
 
+public class Main {
+    public static void main(String[] args) {
+        // Tworzenie instancji klasy Worker
+        Worker worker1 = new Worker("Employee1", 1347.01, 101);
+        Worker worker2 = new Worker("Employee2", 2355.01, 102);
+        Worker worker3 = new Worker("Employee3", 6457.01, 103);
+        Worker worker4 = new Worker("Employee4", 3252.01, 104);
+
+        Manager manager = new Manager("manager", 2640.0, 201);
+
+        System.out.println("Salary of worker1: " + worker1.getSalary());
+        worker1.work();
+
+        System.out.println("Salary of worker2: " + worker2.getSalary());
+        worker2.work();
+
+        System.out.println("Salary of worker3: " + worker3.getSalary());
+        worker3.work();
+
+        System.out.println("Salary of worker4: " + worker4.getSalary());
+        worker4.work();
+
+        System.out.println("Salary of manager: " + manager.getSalary());
+        manager.work();
+    }
+}
